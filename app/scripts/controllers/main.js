300985642ea22f07fc0813027da0c3dea370d925
Leandro Balmaceda <balmacedalm@gmail.com>
2015-07-09 16:13:06 -0300

    First commit. Yeoman's default project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cabinetWebAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cabinetWebAppApp
+ */
+angular.module('cabinetWebAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
