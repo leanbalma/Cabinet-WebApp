fc04c087d03e8fff22c5c5f69ab3d5ca078b8b7e
Leandro Balmaceda <balmacedalm@gmail.com>
2015-07-12 16:12:32 -0300

    Added show node data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,46 @@
  */
 angular.module('cabinetWebAppApp')
   .controller('MainCtrl', function( $scope, cabinetServices ) {
+    $scope.nodeData = {};
+    $scope.inputType = 'password';
+
+    function arrayToTreeData( inputArray ) {
+      console.log('inputArray/outputData');
+      console.log(inputArray);
+      var outputData = [];
+      for( var element in inputArray ) {
+        outputData.push( { 'name': inputArray[element], 'children': [] } );
+      }
+      console.log(outputData);
+      return outputData;
+    }
+
+    function handleNodeData( inputData ) {
+      /* inputData example:
+       * 'my-company/admin-mail': ('account: admin@my-company.com\n'
+       * 'password: qwertyuiop'),
+       */
+      var outputData = {};
+      var arrayOfNodeData = inputData.split('\n');
+      for( var data in arrayOfNodeData ) {
+        if(arrayOfNodeData[data].indexOf('account: ') > -1) {
+          // The data is the account name.
+          var accountName = arrayOfNodeData[data].replace('account: ', '');
+          outputData['account'] = accountName;
+        }
+        else if(arrayOfNodeData[data].indexOf('password: ') > -1) {
+          // The data is the account name.
+          var password = arrayOfNodeData[data].replace('password: ', '');
+          outputData['password'] = password;
+        }
+        else {
+          outputData['password'] = arrayOfNodeData[data];
+        }
+      }
+      console.log('data to print');
+      console.log(outputData);
+      return outputData;
+    }
 
     $scope.treeOptions = {
       nodeChildren: "children",
@@ -26,27 +66,34 @@ angular.module('cabinetWebAppApp')
     };
 
     $scope.dataForTheTree =
-      [
-      { "name" : "Joe", "age" : "21", "children" : [
-        { "name" : "Smith", "age" : "42", "children" : []  },
-        { "name" : "Gary", "age" : "21", "children" : [
-          { "name" : "Jenifer", "age" : "23", "children" : [
-            { "name" : "Dani", "age" : "32", "children" : []  },
-            { "name" : "Max", "age" : "34", "children" : []  }
-
-          ] }
-
-        ] }
-
-      ] },
-      { "name" : "Albert", "age" : "33", "children" : []  },
-      { "name" : "Ron", "age" : "29", "children" : []  }
-
-    ];
+      [];
+    //   [
+    //   { "name" : "Joe", "age" : "21", "children" : [
+    //     { "name" : "Smith", "age" : "42", "children" : []  },
+    //     { "name" : "Gary", "age" : "21", "children" : [
+    //       { "name" : "Jenifer", "age" : "23", "children" : [
+    //         { "name" : "Dani", "age" : "32", "children" : []  },
+    //         { "name" : "Max", "age" : "34", "children" : []  }
+    //
+    //       ] }
+    //
+    //     ] }
+    //
+    //   ] },
+    //   { "name" : "Albert", "age" : "33", "children" : []  },
+    //   { "name" : "Ron", "age" : "29", "children" : []  }
+    //
+    // ];
 
     $scope.showSelected = function(sel) {
       // $scope.selectedNode = sel;
-      console.log('slected: ' + sel);
+      console.log('slected: ');
+      console.log(sel);
+      cabinetServices.getNodeData(sel).success( function(data) {
+        console.log('Node data');
+        console.log( data );
+        $scope.nodeData = handleNodeData( data );
+      });
 
     };
 
@@ -54,7 +101,17 @@ angular.module('cabinetWebAppApp')
       cabinetServices.getNodes().success( function(data) {
         console.log('from controller:');
         console.log(data);
-      } );
+        $scope.dataForTheTree = arrayToTreeData( data );
+      });
+    };
+
+    $scope.showPasswordClicked = function() {
+      if( $scope.inputType === 'password' ) {
+        $scope.inputType = 'text';
+      }
+      else {
+        $scope.inputType = 'password';
+      }
     };
 
   });
--- app/scripts/services/cabinetservices.js
+++ app/scripts/services/cabinetservices.js
@@ -12,19 +12,26 @@ angular.module('cabinetWebAppApp')
     // The host where the rest service is.
     var cabinetHost = 'http://localhost:8000';
 
+
+
     return {
       // Returns all nodes.
       getNodes: function() {
-        var promise = $http.get(cabinetHost + '/vault/nodes')
-        .success(function(data) {
-          console.log('getNodes success!');
-          console.log(data);
-          return data;
-        })
-        .error( function(error) {
-          console.log('getNodes error!');
-          console.log(error);
-        });
+        var promise = $http.get(cabinetHost + '/vault/nodes');
+        // .success(function(data) {
+        //   console.log('getNodes success!');
+        //   console.log(data);
+        //   return arrayToTreeData( data );
+        // })
+        // .error( function(error) {
+        //   console.log('getNodes error!');
+        //   console.log(error);
+        // });
+        return promise;
+      },
+      getNodeData: function( nodeSelected ) {
+        var node = nodeSelected.name.replace('/','_');
+        var promise = $http.get(cabinetHost + '/vault/nodes/' + node);
         return promise;
       }
     };
